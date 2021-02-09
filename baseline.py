3dc2c6dd5be6218679a5c14812ad8fffa0893ae9
mfriebel <matfriebel@gmail.com>
2021-02-09 14:28:55 +0100

    change model

--- baseline.py
+++ baseline.py
@@ -1,10 +1,8 @@
-from sklearn.datasets import load_wine
+from sklear.ensemble import RandomForestClassifier
 import pandas as pd
 
-def train_model():
-    d = load_wine()
-    print(d['DESCR'])
-    X = pd.DataFrame(d['data'], columns=d['feature_names'])
-    y = d['target']  # cultivator
-
-train_model()
+def train_model(X,y):
+    m = RandomForestClassifier()
+    m.fit(X,y)
+    y_pred = m.predict(X)
+    return m
